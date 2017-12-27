98bf415d88e90b927194ff426a6d03acc05d6f6c
atefehrahimi <34093112+atefehrahimi@users.noreply.github.com>
2017-12-27 15:48:07 +0330

    main.py #3
    add while loop for function telnet ()

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from lib import ping
+import lib
 import time
 
-
+result = 1         # for
 if __name__ == "__main__":
     while True:
-        result = ping()
-        print("the result is" + str(result))
-        time.sleep(3)
+
+        while result == 1:     # for check result with ping
+            result = lib.ping()
+            time.sleep(3)
+
+        result = 1            # change result , for check next function(telnet)
+
+        while result == 1:    # for check result with Telnet
+            result = lib.telnet()
+            print(result)     # for check my result , it is not essential
+            time.sleep(3)
