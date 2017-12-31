436410503a286017be03437313d1c65f70c0ae4a
atefehrahimi <34093112+atefehrahimi@users.noreply.github.com>
2017-12-31 10:51:01 +0330

    lib.py #4
    add host & port function for config file

--- lib.py
+++ lib.py
@@ -1,24 +1,40 @@
 import os
 import telnetlib
-hostname = '37.228.138.115'  # sample_ip for test
+import configparser
+
+config = configparser.ConfigParser()
+config.read("sample.ini")
 
 
-def ping():
+def ping(hostname):
     response = os.system("ping -c 2 " + hostname)
-
-    #and then check the response...
-    if response == 0:
+    if response == 0:             # and then check the response...
         result = 1
     else:
         result = 0
     return result
 
 
-def telnet():       # function for telnet ip
-    port = 80
+def telnet(hostname, port):       # function for telnet ip
     try:
-        telnet = telnetlib.Telnet()
-        open_tel = telnet.open(hostname, port)
-        return 1
+        tel = telnetlib.Telnet()
+        tel.open(hostname, port)
+        result = 1
     except IOError:
-        return 0
+        result = 0
+    return result
+
+
+def ports(section):               # function for return ports of sample.ini in a list
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+            dict1[option] = config.get(section, option)
+    list_ports = dict1.values()
+    return list_ports
+
+
+def host():                       # function for return name of my section that is server
+    list_host = config.sections()
+    item_host = list_host[0]
+    return item_host
