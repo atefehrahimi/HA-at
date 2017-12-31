74be0514890b450b51f9cfddcae73db0fc369a9e
atefehrahimi <34093112+atefehrahimi@users.noreply.github.com>
2017-12-31 10:49:13 +0330

    main.py #4
    add config file

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import lib
 import time
 
-result = 1         # for
-if __name__ == "__main__":
-    while True:
 
-        while result == 1:     # for check result with ping
-            result = lib.ping()
+result = 1                         # for done the first while loop
+hostname = lib.host()              # return name of server
+ports = lib.ports(hostname)        # return ports
+
+
+for port_choice in ports:         # a loop for that check ports , if every port was up the loop break
+    result = lib.telnet(hostname, port_choice)
+    if result == 1:
+        port = port_choice
+        break
+    else:
+        continue
+
+
+while True:
+        while result == 1:       # for check result with ping
+            result = lib.ping(hostname)
             time.sleep(3)
 
         result = 1            # change result , for check next function(telnet)
 
         while result == 1:    # for check result with Telnet
-            result = lib.telnet()
+            result = lib.telnet(hostname, port)
             print(result)     # for check my result , it is not essential
             time.sleep(3)
