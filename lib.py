f61548c479ba6053039633a29a056145406aaf77
atefehrahimi <34093112+atefehrahimi@users.noreply.github.com>
2018-01-02 09:13:23 +0330

    lib.py #5
    add function check_ports

--- lib.py
+++ lib.py
@@ -1,21 +1,17 @@
 import os
 import telnetlib
-import configparser
-
-config = configparser.ConfigParser()
-config.read("sample.ini")
 
 
 def ping(hostname):
     response = os.system("ping -c 2 " + hostname)
-    if response == 0:             # and then check the response...
+    if response == 0:                                # and then check the response...
         result = 1
     else:
         result = 0
     return result
 
 
-def telnet(hostname, port):       # function for telnet ip
+def telnet(hostname, port):                         # function for telnet ip
     try:
         tel = telnetlib.Telnet()
         tel.open(hostname, port)
@@ -25,16 +21,11 @@ def telnet(hostname, port):       # function for telnet ip
     return result
 
 
-def ports(section):               # function for return ports of sample.ini in a list
-    dict1 = {}
-    options = config.options(section)
-    for option in options:
-            dict1[option] = config.get(section, option)
-    list_ports = dict1.values()
-    return list_ports
-
-
-def host():                       # function for return name of my section that is server
-    list_host = config.sections()
-    item_host = list_host[0]
-    return item_host
+def check_ports(hostname, port_option):           # function for check that which port is not closed
+    port = None                                   # if all of port was close , return None
+    for port_choice in port_option:               # a loop for that check ports , if every port was up, the loop break
+        result = telnet(hostname, port_choice)
+        if result == 1:
+            port = port_choice
+            break
+    return port
